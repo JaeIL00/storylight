45642cbe66ccf56eb4d709d1b430142efc4e732f
ShinJaeIl01 <ekvm555@naver.com>
2024-10-09 15:10:38 +0900

    story 페이지 기초 구축

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -2,12 +2,14 @@
 
 import { apiStartStory } from "@/app/api/start-story/func";
 import LevelBar from "@/components/story/LevelBar";
+import WritingStoryInput from "@/components/story/WritingStory";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useState } from "react";
 
 const StoryPage = () => {
-  const [todayDate] = useState(dayjs().format("YYYY-MM-DD"));
+  const [todayDate] = useState<string>(dayjs().format("YYYY-MM-DD"));
+  const [isWritingStory, setIsWritingStory] = useState<boolean>(false);
 
   const { data } = useQuery({
     queryKey: ["state_story"],
@@ -19,7 +21,12 @@ const StoryPage = () => {
     <main>
       <LevelBar />
       <article>
-        <section>{data?.content}</section>
+        <section>
+          {isWritingStory ? <WritingStoryInput /> : <p>{data?.content}</p>}
+          <button onClick={() => setIsWritingStory((prev) => !prev)}>
+            글작성
+          </button>
+        </section>
         <section>다음 단계 이야기</section>
       </article>
     </main>
--- src/components/story/WritingStory.tsx
+++ src/components/story/WritingStory.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import { useState } from "react";
+
 const WritingStoryInput = () => {
-  return <></>;
+  const [storyValue, setStoryValue] = useState<string>("");
+  return (
+    <div>
+      <input
+        value={storyValue}
+        onChange={({ target }) => setStoryValue(target.value)}
+      />
+    </div>
+  );
 };
 
 export default WritingStoryInput;
